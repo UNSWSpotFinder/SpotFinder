e4e5ceb38a9ef311ba07b68cee60e88437a4aaea
Loong Loong <longsizhuo@gmail.com>
2024-03-08 01:35:31 +1100

    Added login functionality and password modification. Implemented password encryption and updated testGorm.go to align with the new data structure after password encryption. Made minor adjustments to enable Docker builds. Transitioned the database from the Mingo version to my own version.

--- BackEnd/Service/User/UserService.go
+++ BackEnd/Service/User/UserService.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+	"golang.org/x/crypto/bcrypt"
 	"google.golang.org/api/gmail/v1"
 	_ "gorm.io/gorm"
 	"net/http"
@@ -99,7 +100,16 @@ func CreateUser(c *gin.Context) {
 		})
 		return
 	}
-	user.Password = request.Password
+
+	// 密码加密
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to hash password",
+		})
+		return
+	}
+	user.Password = string(hashedPassword)
 
 	// 判断邮箱是否已经注册过
 	isRegistry, err := User.CheckEmailAlreadyIn(Service.DB, &user)
@@ -158,7 +168,7 @@ func SendCodeHandler(srv *gmail.Service, c *gin.Context, redisCli *redis.Client)
 
 	err = SendEmail(srv, requestData.Email, "Credential Code", "您的验证码是 "+code)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, "我怀疑你断网了")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "我怀疑你断网了"})
 		return
 	}
 
