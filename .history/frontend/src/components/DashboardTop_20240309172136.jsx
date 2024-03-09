60547e9672a54537bef1d80b212741831c3a51be
shayne <shayne.wx1997@gmail.com>
2024-03-09 23:01:46 +1100

    Dashboard+Profile
    The Dashboard framework has finished and the basic version of Profile page is completed.

---
+++ .history/frontend/src/components/DashboardTop_20240309172136.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import './DashboardTop.css';
+
+const DashboardTop = () => {
+  return (
+    <div className="dashboard-top">
+      <div className="top-nav">
+        <div>logo</div>
+        <>logo</>
+      
+      </div>
+      <div className="content-area">
+        <div className="side-nav">SideNav</div>
+        <div className="main-content">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardTop;
---
+++ .history/frontend/src/components/dashboard_20240309200550.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import DashboardTop from './DashboardTop';
+
+const Dashboard = () => {
+  // 组件的状态和逻辑
+
+  return (
+    <div className='Dashboard'>
+      {/* 组件的JSX结构 */}
+      
+
+      Dashboard
+    </div>
+  );
+}
+
+export default Dashboard;
